bdaa5b98390120b80e3b0c94174e7610b0b7a6e5
lawrence-chege <lawrence.chege@meltwater.org>
2020-01-19 08:40:12 +0000

    write test runner

--- test/index.js
+++ test/index.js
@@ -39,3 +39,48 @@
 }
 
 //run the tests
+_app.runTests = () =>{
+    let errors = [];
+    let success = 0;
+    let limit = _app.countTests();
+    let counter = 0;
+    for(let key in _app.tests[key]){
+        if (_app.tests.hashOwnProperty(key)){
+            let subTests = _app.tests[key];
+            for (let testName in subTests){
+                if(subTests.hashOwnProperty(testName)){
+                    () =>{
+                        let tmpTestName = testName;
+                        let testValue = subTests[testName];
+                        // call the tests
+                        try {
+                            testValue(()=>{
+                                // If it calls back without throwing, then it succeeded
+                                // log it in green
+                                console.log('\x1b[32m%s\x1b[0m',tmpTestName);
+                                counter++;
+                                success++;
+                                if(counter == limit){
+                                    _app.produceTestReport(limit, success, errors);
+                                }
+                            })
+                            
+                        } catch (e) {
+                            errors.push({
+                                'name': testName,
+                                'error': e
+                            })
+                            counter++;
+                            console.log('\x1b[31m%s\x1b[0m', tmpTestName);
+                            if(counter == limit){
+                                _app.produceTestReport(limit, success, errors);
+                            }
+                        }
+
+                    }
+                }
+            }
+
+        }
+    }
+}
